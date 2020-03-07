962f238dac684cc8fc853cde0dde8d9b6074d533
Aletrip <aletrip@msn.com>
2020-03-07 12:38:58 -0300

    teste no mac

---
+++ Aula1.py
@@ -0,0 +1,24 @@
+'''for x in range(80+1):
+    print(x)'''
+
+'''a = 5
+b = 50
+while a < b:
+    print (a)
+    a += 2'''
+
+'''x = 25
+y = 42
+if x > y:
+    print ('Acertei')
+elif x == y:
+    print ('Ok')
+else:
+    print ('Errei')'''
+
+if not ((15>14)and("a" == "b"))or(12*4)<48 and 1 ==1:
+    print ('Passei no if')
+else:
+    print ('Cai no else')
+
+
---
+++ Aula6.py
@@ -0,0 +1,5 @@
+#funções
+def soma (n1, n2):
+    return n1 + n2
+
+soma (10, 14)
