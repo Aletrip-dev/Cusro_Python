7ce16a7883baa6d050fa6b2a1b8a16e95030bda4
admin <admin@admins-MacBook-Pro.local>
2020-03-11 09:54:36 -0300

    atualização na tabela de base de dados

--- DataBase.py
+++ DataBase.py
@@ -13,3 +13,27 @@ def gravar_dados(): #cria método para gravar dados
     sql = """INSERT INTO ALBUMS VALUES ('Alex', 'Linking Park', '14/05/1999', '28/09/2000', 'MP3')"""
     cursor.execute(sql)
     conn.commit()
+
+def gravar_muitos(): #método que grava diversos registros no banco
+    ALBUMS = [('Lucas','Metálica','12/02/2020','14/02/2015', 'DVD'),
+    ('Gabriel','Ironmanden','24/02/1982','12/03/2020','CD')]
+    cursor.executemany("INSERT INTO ALBUMS VALUES(?,?,?,?,?)",ALBUMS)
+    conn.commit()
+
+def atualiza(): #faz UPDATE na tabela e altera o nome
+    sql = """UPDATE ALBUMS SET Titulo = 'Alexandro'
+    WHERE Titulo = 'Alex'
+    """
+    cursor.execute(sql)
+    conn.commit()
+
+def excluir(): #Deleta o registro onde o nome aparece
+    sql = """DELETE FROM ALBUMS WHERE Titulo = 'Lucas'"""
+    cursor.execute(sql)
+    conn.commit()
+
+def listar():
+    sql = """SELECT rowid, * FROM ALBUMS ORDER BY Titulo"""
+    cursor.execute(sql)
+    for row in cursor.fetchall(): #seleciona todas as linhas da tabela
+        print(row) #Imprime tudo
